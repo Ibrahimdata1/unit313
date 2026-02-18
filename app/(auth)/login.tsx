dcee6d36520dd4e51bfed6f66771865175e77610
Ibrahimdata1 <bsk.kosit1@gmail.com>
2026-02-18 07:29:50 +0700

    add login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,25 @@
-import { Text, View } from 'react-native'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import { Alert, Text, View } from 'react-native'
+import { supabase } from '../../lib/supabase'
 export default function LoginScreen(){
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [loading,setLoading] = useState(false)
+    const router = useRouter()
+    const onSignInPress = async()=>{
+        setLoading(true)
+        const {error} = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if(error){
+            Alert.alert('Log in Failed',error.message)
+        }else{
+            router.replace('/(tabs)')
+        }
+        setLoading(false)
+    }
     return(
         <View>
             <Text>test</Text>
